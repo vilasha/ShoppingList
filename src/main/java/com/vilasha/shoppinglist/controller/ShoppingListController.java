87b48ce2898f0a391d17d505d6ed769fb16dc2f4
Maria <mariavya@gmail.com>
2020-06-15 16:21:30 +0100

    Fixes for item delete

--- src/main/java/com/vilasha/shoppinglist/controller/ShoppingListController.java
+++ src/main/java/com/vilasha/shoppinglist/controller/ShoppingListController.java
@@ -5,7 +5,9 @@ import com.vilasha.shoppinglist.persist.ShoppingItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,8 +28,14 @@ public class ShoppingListController {
     }
 
     @PostMapping
-    public String addNewShoppingItem(ShoppingItem item) {
+    public String addNewItem(ShoppingItem item) {
         repository.save(item);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteItem(@PathVariable("id") Long id) {
+        repository.deleteById(id);
         return "redirect:/";
     }
 }
