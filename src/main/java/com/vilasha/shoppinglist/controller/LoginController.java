3801810f29b86748e9c270e827e7bdf61cde9422
Maria <mariavya@gmail.com>
2020-06-16 12:04:19 +0100

    Binding results error fix

--- src/main/java/com/vilasha/shoppinglist/controller/LoginController.java
+++ src/main/java/com/vilasha/shoppinglist/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -33,7 +34,7 @@ public class LoginController {
     }
 
     @PostMapping("/register")
-    public String registerNewUser(@Valid UserRepresentation userRepresentation, BindingResult bindingResult) {
+    public String registerNewUser(@Valid @ModelAttribute("user") UserRepresentation userRepresentation, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             return "register";
         if (!userRepresentation.getPassword().equals(userRepresentation.getRepeatPassword()))
